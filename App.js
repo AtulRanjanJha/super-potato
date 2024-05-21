9f7314c9a784e1ae3ae84f89f9ad8cb32a02fc76
Atul Ranjan jha <99534159+AtulRanjanJha@users.noreply.github.com>
2024-05-21 18:10:10 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import GlobeComponent from './components/Globe';
-import './App.css';
+import GlobeComponent from 'Globe';
+import 'App.css';
 
 function App() {
   return (
